846beec3cbc21574acc6a6dec873c181307e1ae1
Atharva Wadekar <theteckky@gmail.com>
2025-03-01 14:12:30 +0530

    Navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.soja.consumerapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.soja.consumerapp"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -37,6 +37,10 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.navigation.fragment)
+    implementation(libs.lottie)
+    implementation(libs.navigation.ui.ktx)
+    implementation(libs.navigation.ui)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/soja/consumerapp/MainActivity.java
+++ app/src/main/java/com/soja/consumerapp/MainActivity.java
@@ -1,14 +1,22 @@
 package com.soja.consumerapp;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,28 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        bottomNav = findViewById(R.id.bottomNav);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(bottomNav, navController);
+
+        navController.addOnDestinationChangedListener((controller,destination, arguments)->{
+            if(destination.getId() == R.id.nav_home || destination.getId() == R.id.nav_search || destination.getId() == R.id.nav_chat || destination.getId() == R.id.nav_profile)
+                getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+            else
+                getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+            if (!navController.popBackStack()) {
+                finish(); // If no more fragments, close the activity
+            }
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
