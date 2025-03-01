604832f3e6018aa39ff8d6d43349a6013d331377
Atharva Wadekar <theteckky@gmail.com>
2025-03-01 16:13:25 +0530

    Search Bar

--- app/src/main/java/com/soja/consumerapp/SearchFragment.java
+++ app/src/main/java/com/soja/consumerapp/SearchFragment.java
@@ -2,13 +2,57 @@ package com.soja.consumerapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.view.MenuProvider;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.appcompat.widget.SearchView;
 
 public class SearchFragment extends Fragment {
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Add menu provider for handling options menu in fragment
+        requireActivity().addMenuProvider(new MenuProvider() {
+            @Override
+            public void onCreateMenu(@NonNull Menu menu, @NonNull MenuInflater menuInflater) {
+                menu.clear(); // Remove previous menu items
+                menuInflater.inflate(R.menu.search_menu, menu);
+
+                MenuItem searchItem = menu.findItem(R.id.action_search);
+                SearchView searchView = (SearchView) searchItem.getActionView();
+
+                searchView.setQueryHint("Search for Fruits, Vegetables...");
+                searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                    @Override
+                    public boolean onQueryTextSubmit(String query) {
+                        // Handle search submit (e.g., filter list)
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onQueryTextChange(String newText) {
+                        // Handle text change (e.g., dynamic filtering)
+                        return false;
+                    }
+                });
+            }
+
+            @Override
+            public boolean onMenuItemSelected(@NonNull MenuItem menuItem) {
+                return false;
+            }
+        }, getViewLifecycleOwner());
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
